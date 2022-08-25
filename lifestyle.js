e88bfa855ff55783835f1f3941144db443e2ec83
bhawana-web19 <bhawanachadda123@gmail.com>
2022-08-25 20:29:59 +0530

    sidebar done

--- lifestyle.js
+++ lifestyle.js
@@ -16,7 +16,7 @@ fetch('https://newsapi.org/v2/everything?q=lifestyle&apiKey=8310b5752c8c4c30b984
 let container = document.getElementById("lifestyleContent")
 function appendData(data){
   data.forEach(function(el,i){
-    if(i<25){
+    if(i<20){
 
         let div = document.createElement("div");
         div.setAttribute("class","newsDivs");
@@ -52,3 +52,187 @@ function appendData(data){
     }
   })
 }
+
+// top headlines api-->https://newsapi.org/v2/top-headlines/sources?category=general&apiKey=8310b5752c8c4c30b984ed3c77811b9d
+
+fetch('https://newsapi.org/v2/top-headlines?country=in&apiKey=8310b5752c8c4c30b984ed3c77811b9d')
+.then(function(res){
+    res.json().then(function(res){
+        let topData = res.articles;
+        console.log('topData:',topData);
+        appendTopNews(topData);
+    })
+})
+.catch(function(err){
+    console.log(err);
+})
+
+let leftContainer = document.getElementById("topNews");
+function appendTopNews(data){
+ 
+    data.forEach(function(el,i){
+        if(i<11){
+            let div = document.createElement("div");
+            div.setAttribute("class","leftBox")
+
+            let title = document.createElement("a");
+            title.innerText = el.title;
+            title.setAttribute("href",`${el.url}`);
+            title.setAttribute("target","_blank");
+
+
+            let source = document.createElement("p");
+            source.setAttribute("class","p1")
+            source.innerText = el.author;
+
+            let date =  document.createElement("p");
+            date.setAttribute("class","leftDate")
+            date.innerText = el.publishedAt;
+
+            div.append(title,source,date);
+            leftContainer.append(div);
+        }
+    })
+}
+
+
+//Health api--> https://newsapi.org/v2/everything?q=health&apiKey=8310b5752c8c4c30b984ed3c77811b9d
+
+fetch('https://newsapi.org/v2/everything?q=health&apiKey=8310b5752c8c4c30b984ed3c77811b9d')
+.then(function(res){
+    res.json().then(function(res){
+        let healthData = res.articles;
+        console.log('healthData:',healthData);
+        appendHealthNews(healthData)
+    })
+})
+.catch(function(err){
+    console.log(err);
+})
+
+let rightContainer = document.getElementById("healthNews")
+
+function appendHealthNews(data){
+ 
+    data.forEach(function(el,i){
+        if(i<10){
+            let div = document.createElement("div");
+            div.setAttribute("class","rightBox")
+
+            let title = document.createElement("a");
+            title.innerText = el.title;
+            title.setAttribute("href",`${el.url}`);
+            title.setAttribute("target","_blank");
+
+            let source = document.createElement("p");
+            source.setAttribute("class","p1")
+            source.innerText = el.author;
+
+            let date =  document.createElement("p");
+            date.setAttribute("class","rightDate")
+            date.innerText = el.publishedAt;
+            
+            let img = document.createElement("img");
+            img.src = el.urlToImage;
+
+            div.append(title,source,date,img);
+            rightContainer.append(div);
+        }
+    })
+}
+
+              // <---------bitcoin data ---------->//
+
+
+let bitcoinContainer = document.getElementById("bitcoin");
+
+fetch('https://newsapi.org/v2/everything?q=bitcoin&apiKey=8310b5752c8c4c30b984ed3c77811b9d')
+.then(function(res){
+    res.json().then(function(res){
+        let bitcoinData = res.articles;
+        console.log('bitcoinData:',bitcoinData);
+        appendBitcoinNews(bitcoinData)
+    })
+})
+.catch(function(err){
+    console.log(err);
+})
+
+function appendBitcoinNews(data){
+ 
+    data.forEach(function(el,i){
+        if(i<5){
+            let div = document.createElement("div");
+            div.setAttribute("class","bitcoinDiv")
+
+            let title = document.createElement("a");
+            title.innerText = el.title;
+            title.setAttribute("href",`${el.url}`);
+            title.setAttribute("target","_blank");
+
+            let source = document.createElement("p");
+            source.setAttribute("class","p1")
+            source.innerText = el.author;
+
+            let date =  document.createElement("p");
+            date.setAttribute("class","date")
+            date.innerText = el.publishedAt;
+            
+            let img = document.createElement("img");
+            img.src = el.urlToImage;
+
+            div.append(title,source,date,img);
+            bitcoinContainer.append(div);
+        }
+    })
+}
+
+
+            // <--------Technology News Data-------->//
+
+   let techContainer = document.getElementById("techNews");
+
+ fetch('https://newsapi.org/v2/everything?q=technology&apiKey=8310b5752c8c4c30b984ed3c77811b9d')
+.then(function(res){
+    res.json().then(function(res){
+        let techData = res.articles;
+        console.log('techData:',techData);
+        appendTechNews(techData)
+    })
+})
+.catch(function(err){
+    console.log(err);
+})
+
+function appendTechNews(data){
+ 
+    data.forEach(function(el,i){
+        if(i<8){
+            let div = document.createElement("div");
+            div.setAttribute("class","techDiv")
+
+            let title = document.createElement("a");
+            title.innerText = el.title;
+            title.setAttribute("href",`${el.url}`);
+            title.setAttribute("target","_blank");
+
+            let source = document.createElement("p");
+            source.setAttribute("class","p1")
+            source.innerText = el.author;
+
+            let date =  document.createElement("p");
+            date.setAttribute("class","date")
+            date.innerText = el.publishedAt;
+            
+            let img = document.createElement("img");
+            img.src = el.urlToImage;
+
+            div.append(title,source,date,img);
+            techContainer.append(div);
+        }
+    })
+}
+
+   
+   
+
